871d2c9e3b6efebe5052615b71d31f2f28c0ec0d
wafa <wafa070@hotmail.com>
2018-10-04 08:16:02 +0300

    ver1

--- app/src/main/java/com/example/pc/petshop/Pet.java
+++ app/src/main/java/com/example/pc/petshop/Pet.java
@@ -1,4 +1,80 @@
 package com.example.pc.petshop;
 
 public class Pet {
+
+    private String ownerid,age,type,price,id,img,city;
+    public Pet() {}
+    public Pet(Pet p) {
+        this.age=p.age;
+        this.id=p.id;
+        this.type=p.type;
+        this.price=p.price;
+        this.ownerid=p.ownerid;
+        this.img=p.img;
+
+    }
+    public Pet(String ownerid, String age, String type, String price, String id, String img,String c) {
+        this.ownerid = ownerid;
+        this.age = age;
+        this.type = type;
+        this.price = price;
+        this.id = id;
+        this.img=img;
+        this.city=c;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+    public String getOwnerid() {
+        return ownerid;
+    }
+
+    public void setOwnerid(String ownerid) {
+        this.ownerid = ownerid;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
--- app/src/main/java/com/example/pc/petshop/welcome.java
+++ app/src/main/java/com/example/pc/petshop/welcome.java
@@ -1,38 +1,115 @@
 package com.example.pc.petshop;
-
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class welcome  extends AppCompatActivity {
+
+public class welcome  extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     DatabaseReference UDB;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
-//Button b=(Button) findViewById(R.id.aa);
-      //  UDB=FirebaseDatabase.getInstance().getReference("test");
-       // UDB.child("1").setValue("hello");
+     Toolbar toolbar = findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 public void showtext(){
-   // Intent intent = new Intent(welcome.this, signup.class);
-     //startActivity(intent);
+    Intent intent = new Intent(welcome.this, addpet.class);
+     startActivity(intent);
 
 
 }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_public_main, menu);
+        return true;
+    }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_profile) {
+            Intent intent = new Intent(welcome.this, addpet.class);
+            startActivity(intent);
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+
+        } else if (id == R.id.nav_preorder) {
+            /*Intent intent = new Intent(MainActivity.this, postActivity.class);
+            Bundle b=new Bundle();
+            b.putString("id",user);
+            intent.putExtras(b);
+            startActivity(intent);
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+            */
+        }
+        else if (id == R.id.chat) {
+            Intent intent = new Intent(welcome.this, addpet.class);
+            startActivity(intent);
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+
+        }
+        else if (id == R.id.nav_request) {
+            Intent intent = new Intent(welcome.this, addpet.class);
+            startActivity(intent);
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
+        else if (id == R.id.nav_logout) {
+
+            mAuth.signOut();
+            if(mAuth.getCurrentUser() == null){
+                Toast.makeText(this , "تم تسجيل الخروج بنجاح" , Toast.LENGTH_SHORT).show();
+                startActivity(new Intent (this , home.class));
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+
+            }}
+        return false;
+    }
 
 }
